cc280869b430cc609545a0908addcc21a9b27a68
martindeboer <m4ndeb2r@gmail.com>
2019-01-11 20:15:01 +0100

    Klasse aan testproject toegevoegd

---
+++ testprojects/VisualisationTest/src/nl/ou/pkgA/pkgASub1/RankPlusPlus.java
@@ -0,0 +1,38 @@
+package nl.ou.pkgA.pkgASub1;
+
+public class RankPlusPlus {
+	
+	public void complexity1() {
+		System.out.println("0");
+		System.out.println("0");
+		System.out.println("0");
+		System.out.println("0");
+		System.out.println("0");
+		System.out.println("0");
+		System.out.println("0");
+		System.out.println("0");
+		System.out.println("0");
+		System.out.println("0");
+		System.out.println("0");
+		System.out.println("0");
+	}
+	
+	public void complexity1Again() {
+		System.out.println("0"); 
+		System.out.println("0");
+		System.out.println("0"); 
+		System.out.println("0");
+		System.out.println("0"); 
+		System.out.println("0");
+		System.out.println("0"); 
+		System.out.println("0");
+		System.out.println("0"); 
+		System.out.println("0");
+		System.out.println("0"); 
+		System.out.println("0");
+		System.out.println("0"); 
+		System.out.println("0");
+		System.out.println("0"); 
+		System.out.println("0");
+	}
+}
