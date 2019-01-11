79cfce7620f421d36d83829898294bd52b12f20c
martindeboer <m4ndeb2r@gmail.com>
2019-01-11 20:10:28 +0100

    Klasse toegevoegd aan testproject.

---
+++ testprojects/VisualisationTest/src/nl/ou/pkgA/pkgASub1/RankPlus.java
@@ -0,0 +1,105 @@
+package nl.ou.pkgA.pkgASub1;
+
+public class RankPlus {
+	
+	public int complexity21(int a, int b, int c, int d, int e, int f, int g, int h, int i, int j, int k) {
+		if (a > b || b > c || c > d || d > e || e > f || f > g || g > h || h > i || i > j || j > k) 
+			return a > k ? 1 : b > k ? 2 : c > k ? 3 : d > k ? 4 : e > k ? 5 : f > k ? 6 : g > k ? 7 : h > k ? 8 : i > k ? 9 : j > k ? 10 : 11;
+		return 0;
+	}
+
+	public void complexity1() {
+		System.out.println("0"); 
+		System.out.println("0");
+		System.out.println("0"); 
+		System.out.println("0");
+		System.out.println("0"); 
+		System.out.println("0");
+		System.out.println("0"); 
+		System.out.println("0");
+		System.out.println("0"); 
+		System.out.println("0");
+		System.out.println("0"); 
+		System.out.println("0");
+		System.out.println("0"); 
+		System.out.println("0");
+		System.out.println("0"); 
+		System.out.println("0");
+		System.out.println("0"); 
+		System.out.println("0");
+		System.out.println("0"); 
+		System.out.println("0");
+		System.out.println("0"); 
+		System.out.println("0");
+		System.out.println("0"); 
+		System.out.println("0");
+		System.out.println("0"); 
+		System.out.println("0");
+		System.out.println("0"); 
+		System.out.println("0");
+		System.out.println("0"); 
+		System.out.println("0");
+		System.out.println("0"); 
+		System.out.println("0");
+		System.out.println("0"); 
+		System.out.println("0");
+		System.out.println("0"); 
+		System.out.println("0");
+		System.out.println("0"); 
+		System.out.println("0");
+		System.out.println("0"); 
+		System.out.println("0");
+		System.out.println("0"); 
+		System.out.println("0");
+		System.out.println("0"); 
+		System.out.println("0");
+		System.out.println("0"); 
+		System.out.println("0");
+		System.out.println("0"); 
+		System.out.println("0");
+		System.out.println("0"); 
+		System.out.println("0");
+		System.out.println("0"); 
+		System.out.println("0");
+		System.out.println("0"); 
+		System.out.println("0");
+		System.out.println("0"); 
+		System.out.println("0");
+		System.out.println("0"); 
+		System.out.println("0");
+		System.out.println("0"); 
+		System.out.println("0");
+		System.out.println("0"); 
+		System.out.println("0");
+		System.out.println("0"); 
+		System.out.println("0");
+		System.out.println("0"); 
+		System.out.println("0");
+		System.out.println("0"); 
+		System.out.println("0");
+		System.out.println("0"); 
+		System.out.println("0");
+		System.out.println("0"); 
+		System.out.println("0");
+		System.out.println("0"); 
+		System.out.println("0");
+		System.out.println("0"); 
+		System.out.println("0");
+		System.out.println("0"); 
+		System.out.println("0");
+		System.out.println("0"); 
+		System.out.println("0");
+		System.out.println("0"); 
+		System.out.println("0");
+		System.out.println("0"); 
+		System.out.println("0");
+		System.out.println("0"); 
+		System.out.println("0");
+		System.out.println("0"); 
+		System.out.println("0");
+		System.out.println("0"); 
+		System.out.println("0");
+		System.out.println("0"); 
+		System.out.println("0");
+	}
+}
